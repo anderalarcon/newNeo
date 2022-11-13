589698ca0569dcdcf54ddfb74e01c5dd6c9a7836
Anderson Achata Alarcon <57463092+anderalarcon@users.noreply.github.com>
2022-11-13 01:28:44 -0500

    [ADD & UPDATE] add solutions component (#1)

---
+++ components/Solutions/Solutions.jsx
@@ -0,0 +1,21 @@
+import Card from './_children/Card/Card.jsx'
+import { solutionsPeru } from '../../utilities/home/contact'
+import style from './Solutions.module.scss'
+const Solutions = () => {
+  return (
+    <section className={style.solutions}>
+      <div className={style.solutions_container}>
+        <h2 className={style.solutions_container_title}>Contamos con soluciones en</h2>
+        <div className={style.solutions_container_services}>
+          {solutionsPeru.map(({ iconSrc, service, description, url }) => {
+            return (
+              <Card iconSrc={iconSrc.src} service={service} key={service} description={description} url={url} />
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Solutions
--- pages/localized_files_2/es_pe/pe.jsx
+++ pages/localized_files_2/es_pe/pe.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Contact from '../../../components/Contact/Contact'
+import Solutions from '../../../components/Solutions/Solutions'
 
 const pe = () => {
   return (
     <>
       <p>Header</p>
       <p>Hero</p>
-      <p> Soluciones</p>
+      <Solutions/>
       <p>Partners</p>
       <p>CDE</p>
       <p>Clientes</p>
       <p>Empresas</p>
+      <p>regional</p>
+      <p>bcorp</p>
+      <p>preguntas frec</p>
       <Contact />
+      <p>contenido</p>
+      <p>footer</p>
     </>
   )
 }
