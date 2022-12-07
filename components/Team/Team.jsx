a77de74784d564eb2d9128c8f9656f54ce8c81cc
Anderson Achata Alarcon <57463092+anderalarcon@users.noreply.github.com>
2022-12-06 21:40:06 -0500

    Team component (#3)
    * [ADD] mobile team component and card
    
    * [ADD] Team component and fix margin on sections

---
+++ components/Team/Team.jsx
@@ -0,0 +1,110 @@
+import style from './Team.module.scss'
+import { Navigation, Pagination, EffectCoverflow } from 'swiper'
+import PropTypes from 'prop-types'
+import { Swiper, SwiperSlide } from 'swiper/react'
+// Import Swiper styles
+import 'swiper/css'
+// import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import leftArrow from '../../public/assets/Swiper/left-arrow-accent.svg'
+import rightArrow from '../../public/assets/Swiper/right-arrow-accent.svg'
+import Profile from './_children/Profile/Profile'
+import { useEffect, useState } from 'react'
+// import { team } from '../../utilities/crm/team'
+import uuid from 'react-uuid'
+
+const Team = ({ team }) => {
+  const [isMobile, setIsMobile] = useState(
+    typeof window === 'undefined' ? 0 : window.innerWidth < 1200
+  )
+  const updateDimensions = (event) => {
+    if (typeof window !== 'undefined') {
+      setIsMobile(event.target.innerWidth < 1200)
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('resize', updateDimensions)
+    return () => window.removeEventListener('resize', updateDimensions)
+  }, [updateDimensions])
+
+  // console.log(width)
+  return (
+    <div className={style.team}>
+      <div className={style.team_container}>
+        <h1 className={style.team_container_title}>Conoce a nuestro equipo</h1>
+        <p className={style.team_container_subtitle}>
+          Nuestros consultores expertos en CRM y Salesforce te ayudarán a
+          cumplir los objetivos de tu negocio.
+        </p>
+        <div
+          className={`${style.team_container_swiper_button} ${style.swiper_button_next_cases}`}
+        >
+          <span className={style.team_container_swiper_button_span}>
+            Siguiente
+          </span>
+          <img src={rightArrow.src} alt='Casos de Exito Neo Consulting' />
+        </div>
+        <div
+          className={`${style.team_container_swiper_button} ${style.swiper_button_prev_cases}`}
+        >
+          <img src={leftArrow.src} alt='Casos de Exito Neo Consulting' />
+          <span className={style.team_container_swiper_button_span}>
+            Anterior
+          </span>
+        </div>
+        <div className='swiper-pagination-teams'></div>
+        <Swiper className={style.team_container_special}
+          modules={[Navigation, Pagination, EffectCoverflow]}
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={isMobile ? '1' : '3'}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false
+          }}
+          // spaceBetween={24}
+          initialSlide="2"
+          spaceBetween={48}
+          navigation={{
+            nextEl: `.${style.swiper_button_next_cases}`,
+            prevEl: `.${style.swiper_button_prev_cases}`,
+            disabledClass: 'swiper-button-disabled'
+          }}
+          pagination={{
+            clickable: true,
+            el: '.swiper-pagination-teams',
+            type: 'bullets',
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '"></span>'
+            }
+          }}
+          // loop={true}
+        >
+          {team.map(({ fullname, charge, img, bio, linkedin }) => {
+            return (
+              <SwiperSlide key={uuid()} className='slide-team'>
+                <Profile
+                  fullName={fullname}
+                  charge={charge}
+                  img={img}
+                  bio={bio}
+                  linkedIn={linkedin}
+                />
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
+    </div>
+  )
+}
+
+Team.propTypes = {
+  team: PropTypes.array
+}
+
+export default Team
---
+++ components/Team/_children/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import style from './Profile.module.scss'
+import PropTypes from 'prop-types'
+import linkedin from '../../../../public/assets/Team/linkedin.svg'
+const Profile = ({ img, fullName, bio, linkedIn, charge }) => {
+  return (
+    <div className={style.profile}>
+      <div className={style.profile_container}>
+        <div className={style.profile_container_top}>
+          <img
+            className={style.profile_container_top_img}
+            src={img}
+            alt={fullName}
+          />
+        </div>
+        <div className={style.profile_container_bottom}>
+          <p className={style.profile_container_bottom_fullname}>{fullName}</p>
+          <p className={style.profile_container_bottom_charge}>{charge}</p>
+          <p className={style.profile_container_bottom_bio}>{bio}</p>
+          <div className={style.profile_container_bottom_social}>
+            <p className={style.profile_container_bottom_social_linked}>
+              Linkedin
+            </p>
+            <img
+              className={style.profile_container_bottom_social_logo}
+              src={linkedin.src}
+              alt={'linkedin' + fullName}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Profile.propTypes = {
+  img: PropTypes.string,
+  fullName: PropTypes.string,
+  bio: PropTypes.string,
+  linkedIn: PropTypes.string,
+  charge: PropTypes.string
+}
+
+export default Profile
