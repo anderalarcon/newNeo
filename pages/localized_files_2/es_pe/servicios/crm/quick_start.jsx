bcf26384ec0fcc994ae680bb21e72651585577c7
Anderson Achata Alarcon <57463092+anderalarcon@users.noreply.github.com>
2022-12-16 23:44:48 -0500

    [ADD] structure to crm pages and some fixes (#7)

--- pages/localized_files_2/es_pe/servicios/crm/quick_start.jsx
+++ pages/localized_files_2/es_pe/servicios/crm/quick_start.jsx
@@ -1,19 +1,22 @@
 /* eslint-disable */
 
-import Contact from "../../../../../components/Contact/Contact"
-import Questions from "../../../../../components/Questions/Questions"
-import SuccessCase from "../../../../../components/SuccessCase/SuccessCase"
-import Team from "../../../../../components/Team/Team"
+import Contact from '../../../../../components/Contact/Contact'
+import Hero from '../../../../../components/Crm/Hero/Hero'
+import Questions from '../../../../../components/Questions/Questions'
+import SuccessCase from '../../../../../components/SuccessCase/SuccessCase'
+import Team from '../../../../../components/Team/Team'
+import { dataHeroQuick } from '../../../../../utilities/crm/hero'
 import { questionsCrm } from '../../../../../utilities/crm/questions'
 import { team } from '../../../../../utilities/crm/team'
 
 const quick_start = () => {
   return (
     <>
-    <Team team={team}/>
-    <SuccessCase/>
-    <Questions questions={questionsCrm}/>
-    <Contact service={'crm'} solution={'quick-start'}/>
+      <Hero data={dataHeroQuick} isSolution={true} />
+      <Team team={team} />
+      <SuccessCase />
+      <Questions questions={questionsCrm} />
+      <Contact service={'crm'} solution={'quick-start'} />
     </>
   )
 }
---
+++ utilities/crm/hero.js
@@ -0,0 +1,97 @@
+import hero from '../../public/assets/Crm/Hero/hero.jpg'
+import salesforce from '../../public/assets/Crm/Hero/salesforce.svg'
+import salesforcemkt from '../../public/assets/Crm/Hero/salesforce-mkt.svg'
+
+import ga from '../../public/assets/Crm/Hero/FullService/ga.svg'
+import gmp from '../../public/assets/Crm/Hero/FullService/gmp.svg'
+import gads from '../../public/assets/Crm/Hero/FullService/gads.svg'
+import gc from '../../public/assets/Crm/Hero/FullService/gc.svg'
+
+export const dataHero = {
+  title: 'Somos especialistas en',
+  subtitle: 'CRM',
+  service: 'crm',
+  solution: 'general',
+  desc: 'Crea, desarrolla y mejora la relación con tus clientes.',
+  descCert: 'Nuestros consultores de CRM están certificados en:',
+  mainImg: hero.src,
+  certifications: [
+    {
+      src: salesforce.src
+    },
+    {
+      src: salesforcemkt.src
+    }
+  ]
+}
+
+export const dataHeroFull = {
+  title: 'Full Service CRM',
+  subtitle: '',
+  service: 'crm',
+  solution: 'full-service-crm',
+  desc: 'Servicio 360° donde partimos de la estrategia de CRM y posterior operación del CRM, con gestión de campañas y comunicación cross-channel, basado en optimizaciones data-driven.',
+  descCert: 'Nuestros analistas están certificados en:',
+  mainImg: hero.src,
+  certifications: [
+    {
+      src: ga.src
+    },
+    {
+      src: gmp.src
+    },
+    {
+      src: gads.src
+    },
+    {
+      src: gc.src
+    }
+  ]
+}
+
+export const dataHeroAccelerator = {
+  title: 'Accelerator',
+  subtitle: '',
+  service: 'crm',
+  solution: 'accelerator',
+  desc: 'Servicio 360° donde partimos de la estrategia de CRM y posterior operación del CRM, con gestión de campañas y comunicación cross-channel, basado en optimizaciones data-driven.',
+  descCert: 'Nuestros analistas están certificados en:',
+  mainImg: hero.src,
+  certifications: [
+    {
+      src: ga.src
+    },
+    {
+      src: gmp.src
+    },
+    {
+      src: gads.src
+    },
+    {
+      src: gc.src
+    }
+  ]
+}
+export const dataHeroQuick = {
+  title: 'Quick Start',
+  subtitle: '',
+  service: 'crm',
+  solution: 'quick-start',
+  desc: 'Servicio 360° donde partimos de la estrategia de CRM y posterior operación del CRM, con gestión de campañas y comunicación cross-channel, basado en optimizaciones data-driven.',
+  descCert: 'Nuestros analistas están certificados en:',
+  mainImg: hero.src,
+  certifications: [
+    {
+      src: ga.src
+    },
+    {
+      src: gmp.src
+    },
+    {
+      src: gads.src
+    },
+    {
+      src: gc.src
+    }
+  ]
+}
