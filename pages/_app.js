a2e26e29545e4274575113375c018624f1e23d3a
Ander <20172991@aloe.ulima.edu.pe>
2023-02-13 15:01:18 -0500

    linked in configuration to hubspot

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Script from 'next/script'
 import '../styles/globals.scss'
 // eslint-disable-next-line
-function MyApp ({ Component, pageProps }) {
-  // eslint-disable-next-line
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/21198826.js"/>
+    </>
+  )
 }
 
 export default MyApp
